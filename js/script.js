c862fc340d61f41502f83905c8a44b4fc5390609
Thiago Oliveira <thiagosilvaoliveira66@gmail.com>
2018-10-05 13:23:54 -0300

    Tornando canvas responsivo

--- js/script.js
+++ js/script.js
@@ -1,63 +1,64 @@
 var context;
-
+var canvas;
 $(document).ready(function() {
     $("#corrida").css("border",0).css("padding", 0).css("margin", 0);
-    $("#corrida").attr("width",$(document).width()).attr("height",$(document).height());
-    var canvas = document.getElementById("corrida");
-    criaJogo(canvas);    
+    $("#corrida").attr("width",$(window).width()).attr("height",$(window).height());
+    canvas = document.getElementById("corrida");
+    criaJogo();    
     
     
 });
 $(window).bind("resize",function() {
-    $("#corrida").attr("width",$(document).width()).attr("height",$(document).height());
-    console.log("resize");
-    criaJogo(document.getElementById("corrida"));
+    $("#corrida").attr("width",$(window).width()).attr("height",$(window).height());
+    canvas.width = $("#corrida").width();
+    canvas.height = $("#corrida").height();
+    console.log(canvas);
+    criaJogo();
 });
 
-function criaJogo(canvas) {
-    console.log("desenho");
+function criaJogo() {
     context = canvas.getContext('2d');
     var quadradosGuia = canvas.height / 8;
     //Desenhando grama
     // context.beginPath();
     context.fillStyle = "#63ca00";
-    context.fillRect(0, 0, 250, canvas.height);
-    context.fillRect(canvas.width - 250, 0, 250, canvas.height);
+    context.fillRect(0, 0, canvas.width / 6.8, canvas.height);
+    context.fillRect(canvas.width - (canvas.width / 6.8), 0, (canvas.width / 6.8), canvas.height);
 
     //Desenhando guias
     
     //Esquerda
-    context.beginPath();
-    context.moveTo(300,0);
-    context.lineTo(300, canvas.height);
+    // context.beginPath();
+    // context.moveTo(canvas.width / 5.45,0);
+    // context.lineTo(canvas.width / 5.45, canvas.height);
     for (var i = 0; i < 8; i++) {
         if (i % 2 == 0) {
             context.fillStyle = "red";
-            context.fillRect(250,(i * quadradosGuia), 50, quadradosGuia);
+            context.fillRect(canvas.width / 6.8,(i * quadradosGuia), canvas.width / 27, quadradosGuia);
         } else {
             context.fillStyle = "white";
-            context.fillRect(250,(i * quadradosGuia), 50, quadradosGuia);
+            context.fillRect(canvas.width / 6.8,(i * quadradosGuia), canvas.width / 27, quadradosGuia);
         }
     }
     
 
     //Direita
-    context.moveTo(canvas.width - 250, 0);
-    context.lineTo(canvas.width - 250, canvas.height);
+    // context.moveTo(canvas.width - 250, 0);
+    // context.lineTo(canvas.width - 250, canvas.height);
     for (var i = 0; i < 8; i++) {
         if (i % 2 == 0) {
             context.fillStyle = "red";
-            context.fillRect(canvas.width - 250,(i * quadradosGuia), 50, quadradosGuia);
+            context.fillRect(canvas.width - (canvas.width / 6.8),(i * quadradosGuia), canvas.width / 27, quadradosGuia);
         } else {
             context.fillStyle = "white";
-            context.fillRect(canvas.width - 250,(i * quadradosGuia), 50, quadradosGuia);
+            context.fillRect(canvas.width - (canvas.width / 6.8),(i * quadradosGuia), canvas.width / 27, quadradosGuia);
         }
     }
 
-    context.lineWidth = 1;
-    context.strokeStyle = 'red';
+    // context.lineWidth = 1;
+    // context.strokeStyle = 'red';
 
-    context.stroke();
+    // context.stroke();
 
     //Desenhando na rua as faixas
     context.fillStyle = "white";
